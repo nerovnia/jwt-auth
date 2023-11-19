d685ea7a16df32e478abe5dc34cf27f931b885f1
Volodymyr Nerovnia <nerv@i.ua>
2023-11-19 02:50:19 +0200

    Created: - prisma service - auth module, service and controller

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '.prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient, implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super(
+      datasources: {
+      db: {
+        url: ''
+      }
+    }
+    );
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
